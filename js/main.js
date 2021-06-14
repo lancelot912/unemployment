b325b42de69e50d4ee676415698051b572b77841
lancelot912 <63931131+lancelot912@users.noreply.github.com>
2021-06-13 17:44:24 -0700

    main

--- js/main.js
+++ js/main.js
@@ -7,9 +7,11 @@ var map = L.map('map',{
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/light-v9',
+    id: 'mapbox/dark-v9',
     tileSize: 512,
     zoomOffset: -1,
+
+    
     }).addTo(map);
     
  
